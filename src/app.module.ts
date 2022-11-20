a403e99906995d67034812f7ec60e465d1cf8657
Ali Gamal <ali.gamal@beyond-solution.com>
2022-11-20 18:55:41 +0200

    NF: update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminAuthModule } from './authentication/admin/admin.auth.module';
 import { ClientAuthModule } from './authentication/client/client.auth.module';
+import { OwnerAuthModule } from './authentication/owner/owner.auth.module';
 import { SharedAuthModule } from './authentication/shared/auth.module';
 
 @Module({
@@ -13,6 +15,8 @@ import { SharedAuthModule } from './authentication/shared/auth.module';
     }),
     SharedAuthModule,
     ClientAuthModule,
+    AdminAuthModule,
+    OwnerAuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
